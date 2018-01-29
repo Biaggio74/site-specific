6e6b85f1593553164a932a332fb2c158153a365d
BKI <b.kisfali@gmail.com>
2018-01-30 00:47:26 +0100

    change port to process port

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.get('/', (req, res, next) => {
   res.status(200).send('App is up and running ;)');
