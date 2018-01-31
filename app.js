396a2e5226e37170300c08d698a6dbe2322e69dd
BKI <b.kisfali@gmail.com>
2018-01-31 18:15:33 +0100

    add favicon and static router

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const helmet = require('helmet');
+//logging middleware
+const morgan = require('morgan');
+const favicon = require('serve-favicon');
+
+const staticRouter = require('./server/routes/static.js');
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(morgan('dev'));
+
+app.set('views', path.join(__dirname, 'client/views/pages'));
+app.set('view engine', 'ejs');
+
+app.use('/', staticRouter);
 
 const PORT = process.env.PORT || 3000;
 
 app.use(helmet());
-
-app.get('/', (req, res, next) => {
-  console.log('GET request OK....')
-  res.status(200).send({name: 'appStart'});
-})
 
 
 app.listen(PORT, () => {
---
+++ server/routes/static.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const staticRouter = express.Router();
+
+
+staticRouter.get('/', (req, res, next) => {
+  console.log('GET request OK....')
+  var drinks = [
+        { name: 'Bloody Mary', drunkness: 3 },
+        { name: 'Martini', drunkness: 5 },
+        { name: 'Scotch', drunkness: 10 }
+    ];
+    var tagline = "Any code of your own that you haven't looked at for six or more months might as well have been written by someone else. And one more linnneee";
+
+    res.render('index', {
+        drinks: drinks,
+        tagline: tagline
+    });
+})
+
+staticRouter.get('/about', (req, res, next) => {
+  res.send(`This is page ABOUT`)
+})
+
+
+module.exports = staticRouter;
